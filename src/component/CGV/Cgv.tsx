b800ba77f80068bd6f53524555bcb2f863e888d1
KatiaZd <katia.zouad@hotmail.fr>
2023-07-25 14:20:18 +0200

    feat/footer : création du composant Cgv

---
+++ src/component/CGV/Cgv.tsx
@@ -0,0 +1,45 @@
+import style from './Cgv.module.css';
+
+const Cgv = () => {
+    return (
+        <section className={style.sectionCgv}>
+            <h1>Conditions Générales de Vente</h1>
+            <h2>Restaurant Le Sahara en Bouche</h2>
+
+            <h3>1. Présentation du Restaurant</h3>
+            <p>Le restaurant "Le Sahara en Bouche" est une enseigne fictive proposant une cuisine inspirée de la région saharienne. Nous sommes situés au 14 rue de la Beaune - 93100 Montreuil et sommes heureux de vous accueillir pour découvrir nos plats savoureux.</p>
+
+            <h3>2. Menus et Prix</h3>
+            <p>Nous proposons un menu varié comprenant des entrées, des plats principaux, des grillades, des desserts et des boissons.
+            Les prix des plats et des boissons sont indiqués en euros € et incluent la TVA applicable.
+            Des suppléments peuvent s'appliquer pour certains plats dans la section 'Plus de gourmandise'</p>
+
+            <h3>3. Paiement</h3>
+            <p>Nous acceptons les paiements par cartes de crédit/débit et en espèces</p>
+
+            <h3>4. Horaires d'ouverture</h3>
+            <p>Nous sommes ouverts du lundi au samedi de 11h30 à 23h30 et fermés le dimanche.
+            Les horaires d'ouverture peuvent être modifiés pendant les jours fériés ou les périodes spéciales</p>
+
+            <h3>5. Service Client</h3>
+            <p>Nous nous engageons à offrir un service de qualité et à répondre à toutes vos questions et préoccupations.
+            Si vous avez des plaintes ou des commentaires, veuillez nous en informer immédiatement afin que nous puissions y remédier.</p>
+
+            <h3>6. Responsabilité et Sécurité</h3>
+            <p>Le restaurant "Le Sahara en Bouche" décline toute responsabilité en cas de perte ou de dommages aux biens personnels des clients.
+            Nous mettons en œuvre des mesures de sécurité pour assurer la sécurité de nos clients et de nos employés.</p>
+
+            <h3>7. Confidentialité</h3>
+            <p>Nous respectons la confidentialité des informations personnelles que vous nous fournissez et les utilisons conformément à notre politique de confidentialité. Veuillez consulter notre politique de confidentialité sur notre site Web pour plus d'informations.</p>
+
+            <h3>8. Modification des CGV</h3>
+            <p>Les CGV peuvent être mises à jour de temps à autre. Toute modification sera affichée sur notre site Web et prendra effet immédiatement.</p>
+
+            <h3>9. Dispositions Légales</h3>
+            <p>Le restaurant "Le Sahara en Bouche" est une entreprise fictive. Toute ressemblance avec un établissement réel est purement fortuite.
+            Les présentes CGV sont régies par les lois française. Tout litige sera soumis à la juridiction exclusive des tribunaux compétents de Montreuil.</p>
+        </section>
+    )
+}
+
+export default Cgv;
