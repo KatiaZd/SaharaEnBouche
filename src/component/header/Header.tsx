22e443f6097e43fb758040661a33198682916eab
assa.nianghane <assa.nianghane@renault.com>
2023-07-26 02:34:08 +0200

    header:j'ai mise le logo et le panier en desktop et un menu burger en responsive j'ai utilisé un useState pour le changement d'etat si le menu burger est ouvert on applique une croix pour fermer la nav au clique avec une petite  transition

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,8 +1,18 @@
 import style from "./Header.module.css";
+import { useState } from "react";
 const Header = () => {
+  // descructuration pour savoir si c'est ouvert ou fermer
+  const [showLinks, setShowLinks] = useState(false);
+  const handleShowlinks = () => {
+    // si on est a true on active le burger
+    setShowLinks(!showLinks);
+    console.log("showLinks",showLinks);
+  };
   return (
     <header className={style.header}>
-      <div className={style.container}>
+      {/* faire une condition quand showLinks est a true la class show nav se met en place */}
+      {/* <div className={`${style.container} ${style.navbar} ${style.show_nav}`}> */}
+      <div className={`${style.container} ${style.navbar} ${showLinks?style.show_nav:""}`}>
         <a href="#">
           <img
             className={style.logo}
@@ -10,9 +20,46 @@ const Header = () => {
             alt="logo sahara en bouche"
           />
         </a>
-        <a href="#">
+        <a href="#" className="linkBasket">
           <img className={style.basket} src="assets/icons/basket.png" />
         </a>
+        <nav className={style.nav}>
+          <ul className={`${style.navbar_links} ${style.ul}`}>
+            <li className={style.navbar_item}>
+              <a href="/" className={style.navbar_link}>
+                TOUT
+              </a>
+            </li>
+            <li className={style.navbar_item}>
+              <a href="/" className={style.navbar_link}>
+                ENTREES
+              </a>
+            </li>
+            <li className={style.navbar_item}>
+              <a href="/" className={style.navbar_link}>
+                PLATS
+              </a>
+            </li>
+            <li className={style.navbar_item}>
+              <a href="/" className={style.navbar_link}>
+                GRILLADES
+              </a>
+            </li>
+            <li className={style.navbar_item}>
+              <a href="/" className={style.navbar_link}>
+                DESSERTS
+              </a>
+            </li>
+            <li className={style.navbar_item}>
+              <a href="/" className={style.navbar_link}>
+                BOISSONS
+              </a>
+            </li>
+          </ul>
+          <button className={style.navbar_burger} onClick={()=>handleShowlinks()}>
+            <span className={style.burger_bar}></span>
+          </button>
+        </nav>
       </div>
     </header>
   );
