c1637f2b67d50161438c12934f0c9ffc40c0d2bc
assa.nianghane <assa.nianghane@renault.com>
2023-08-11 14:01:46 +0200

    creation du bonton reutilisable et l'appel sur la page product

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,12 +1,20 @@
+import Button from "../component/Button/Button";
 import BackButton from "../component/BackButton/BackButton";
 import SelectQuantityProduct from "../component/SelectQuantityProduct/SelectQuantityProduct";
-
+import style from './Product.module.css';
 const Product = () => {
+  const addProduct = () => {
+    console.log("ajouter un produit");
+  };
+
   return (
     <>
       <BackButton />
       <h1>Page product </h1>
-      <SelectQuantityProduct />
+      <div className={style.containerButton}>
+        <SelectQuantityProduct />
+        <Button title="Ajouter au panier" callback={addProduct} />
+      </div>
     </>
   );
 };
