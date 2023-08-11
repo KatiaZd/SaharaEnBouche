be50a6dc8db78c959c2bd8e01cb2b273bf09b9eb
assa.nianghane <assa.nianghane@renault.com>
2023-08-11 12:53:09 +0200

    creation du composant  backButton et j'ai fait l'appel du composant dans la page Product

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,10 +1,12 @@
+import BackButton from "../component/BackButton/BackButton";
 import SelectQuantityProduct from "../component/SelectQuantityProduct/SelectQuantityProduct";
 
 const Product = () => {
   return (
     <>
+      <BackButton />
       <h1>Page product </h1>
-      <SelectQuantityProduct/>
+      <SelectQuantityProduct />
     </>
   );
 };
