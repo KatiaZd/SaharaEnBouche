13c2de4e65f11952f9609a91b6a49ad271e8207c
KatiaZd <katia.zouad@hotmail.fr>
2023-08-18 13:12:21 +0200

    page 404

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,6 +2,7 @@ import Product from "./pages/Product";
 import App from "./App";
 import HomePage from "./pages/HomePage";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import NotFound from "./component/NotFound/NotFound";
 
 const AppRouter = () => {
   const router = createBrowserRouter([
@@ -15,7 +16,12 @@ const AppRouter = () => {
         },
         {
           path:"/Product/:id",
-          element:<Product/>
+          element:<Product/>,
+        },
+        // Bien laisser la route 404 en dernier afin de s'assurer qu'il sera utilisée uniquement si aucune routes ne correspond à l'URL
+        {
+          path: "*",
+          element: <NotFound />,
         }
       ],
     },
