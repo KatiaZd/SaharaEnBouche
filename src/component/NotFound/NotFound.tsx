aa83dd393f659d0f11bf8b22eb9de5db48e33db3
assa.nianghane <assa.nianghane@renault.com>
2023-08-29 16:38:19 +0200

    fix la page 404

--- src/component/NotFound/NotFound.tsx
+++ src/component/NotFound/NotFound.tsx
@@ -1,20 +1,32 @@
+import { useNavigate } from "react-router";
+import Button from "../../component/Button/Button";
 import style from "./NotFound.module.css";
 
 const NotFound = () => {
+  let navigate = useNavigate();
   return (
-    <div className={style.div}>
-      <img
-        className={style.imgNotFound}
-        src="../../../public/assets/img/notFound.png"
-        alt="Page 404"
-      />
-      <p className={style.p_notFound}>
-        Oops ! Vous êtes égaré dans les dunes numériques de notre application.
-        <br />
-        Rejoignez-nous au cœur du Sahara en Bouche en explorant nos plats exquis
-        !
-      </p>
+    <><div>
+
+        <img
+          className={style.imgNotFound}
+          src="../../../public/assets/img/notFound.png"
+          alt="Page 404"
+        />
     </div>
+    <main>
+      <div className={style.divNotFound}>
+        <p className={style.p_notFound}>
+          Oops ! Vous êtes égaré dans les dunes numériques de notre application.
+          <br />
+          Rejoignez-nous au cœur du Sahara en Bouche en explorant nos plats
+          exquis !
+        </p>
+         <div className={style.buttonHome}> 
+          <Button  title="Retour à l'accueil" callback={() => navigate("/")} />
+         </div> 
+      </div>
+    </main>
+    </>
   );
 };
 
